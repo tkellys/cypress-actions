420e51f1f4fc9ac5af35dcc68d3b0feaf97f68ac
tkellys <116043363+tkellys@users.noreply.github.com>
2025-06-24 10:50:45 -0300

    Update runner.js

--- runner.js
+++ runner.js
@@ -1,7 +1,7 @@
 const cypress = require('cypress')
 const tesults = require('cypress-tesults-reporter');
 
-const TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImUzMjlmMjY5LTAxYmItNDhkMS04YjQ4LWU2YjU0NGIxMzA5YS0xNzUwNzY4NjM4MTE1IiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiZjc1ZGQxZWYtODNiYi00YWY1LWI1ZDgtZTM1ZDQ5OGUwOTM4IiwidHlwZSI6InQifQ.bI0Xneoq6rV8anISAejJnF60o3efWeTBHqzoHG-t8o4"
+const TOKEN = ""
 cypress.run({
   // specs to run here
 })
